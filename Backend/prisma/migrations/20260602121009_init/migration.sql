832b894efac48612913aac5f225135bb6b81abc3
Prayanshu Chourasia <prayanshuchourasia01@gmail.com>
2026-06-02 17:50:43 +0530

    SuccessFully Databse Connected and Prisma connected successfully

---
+++ Backend/prisma/migrations/20260602121009_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Template" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "department" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- Backend/server.js
+++ Backend/server.js
@@ -2,18 +2,34 @@ import { configDotenv } from 'dotenv';
 import express from 'express';
 import dotenv from 'dotenv' ; 
 import userRoute from './routes/userRoutes.js';
-
-
+import prisma from './src/config/prisma.js';
+// const prisma = require('./src/config/prisma');
 
 dotenv.config();
-
 
 
 const app = express();
 
 const PORT = process.env.PORT || 5000; 
+
+// Test Route for Prisma 
+app.get('/test-db', async (req, res) => {
+    const templates = await prisma.template.findMany();
+
+    res.json({
+        success: true,
+        data: templates
+    });
+});
+
+
+
 // Routes
 app.use('/user',userRoute)
+
+
+
+
 
 // Starting Server 
 app.listen(PORT,()=>{
